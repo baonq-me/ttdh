46eb3a5d671ff57f434c55e17cc8b122a7f305ed
quocbao <quocbao747@gmail.com>
2021-12-12 13:56:55 +0700

    Update suggestTier.js

--- suggestTier.js
+++ suggestTier.js
@@ -1,10 +1,10 @@
     suggestTier(totalScore, scoreBySection) {
       switch (true) {
-        case totalScore > 30 || scoreBySection['S2000'] >= 6:
+        case totalScore > 30 || scoreBySection['S2000'] >= 6:           // total score of S2000 >= 6 
           return 'nc4';
-        case totalScore >= 22 || scoreBySection['S1000'] >= 8:
+        case totalScore >= 22 || scoreBySection['S1000'] >= 8:          // total score of S1000 >= 8
           return 'nc3';
-        case totalScore >= 14 || scoreBySection['S1000'] >= 4:
+        case totalScore >= 14 || scoreBySection['S1000'] >= 4:          // total score of S1000 >= 4
           return 'nc2';
         case totalScore >= 6:
           return 'nc1';
