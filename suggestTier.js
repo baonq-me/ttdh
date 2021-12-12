49fc1d6453771beccaeda55167c8fbd6efbd8ad4
quocbao <quocbao747@gmail.com>
2021-12-12 13:54:03 +0700

    Create suggestTier.js

---
+++ suggestTier.js
@@ -0,0 +1,14 @@
+    suggestTier(totalScore, scoreBySection) {
+      switch (true) {
+        case totalScore > 30 || scoreBySection['S2000'] >= 6:
+          return 'nc4';
+        case totalScore >= 22 || scoreBySection['S1000'] >= 8:
+          return 'nc3';
+        case totalScore >= 14 || scoreBySection['S1000'] >= 4:
+          return 'nc2';
+        case totalScore >= 6:
+          return 'nc1';
+        default:
+          return 'nc0';
+      }
+    },
